e498e254bc6a93dc5dee4a658f31cffd9504191b
erika <erika.milovec2@gmail.com>
2022-06-22 18:29:34 +0200

    Finished project

---
+++ script.js
@@ -0,0 +1,79 @@
+//Password validation---------------------------------------------------
+var password = document.getElementById("psw");
+var letter = document.getElementById("letter");
+var capital = document.getElementById("capital");
+var number = document.getElementById("number");
+var length = document.getElementById("length");
+
+
+// When the user starts to type something inside the password field
+password.onkeyup = function() {
+  // Validate lowercase letters
+  var lowerCaseLetters = /[a-z]/g;
+  if(password.value.match(lowerCaseLetters)) {
+    letter.classList.remove("invalid");
+    letter.classList.add("valid");
+  } else {
+    letter.classList.remove("valid");
+    letter.classList.add("invalid");
+}
+
+  // Validate capital letters
+  var upperCaseLetters = /[A-Z]/g;
+  if(password.value.match(upperCaseLetters)) {
+    capital.classList.remove("invalid");
+    capital.classList.add("valid");
+  } else {
+    capital.classList.remove("valid");
+    capital.classList.add("invalid");
+  }
+
+  // Validate numbers
+  var numbers = /[0-9]/g;
+  if(password.value.match(numbers)) {
+    number.classList.remove("invalid");
+    number.classList.add("valid");
+  } else {
+    number.classList.remove("valid");
+    number.classList.add("invalid");
+  }
+
+  // Validate length
+  if(password.value.length >= 8) {
+    length.classList.remove("invalid");
+    length.classList.add("valid");
+  } else {
+    length.classList.remove("valid");
+    length.classList.add("invalid");
+  }
+}
+
+
+//Confirm password 
+const confirm_password = document.getElementById('psw_confirm')
+
+function validatePassword(){
+  if(password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Passwords Don't Match");
+  } else {
+    confirm_password.setCustomValidity('');
+  }
+}
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
+
+//Login button
+const login = document.getElementById('loginContainer');
+login.onclick = () => loginForm();
+
+function loginForm(){
+    login.classList.remove('loginContainer');
+    login.classList.add('loginForm');
+}
+
+//X button
+const xLogin = document.querySelector('xLogin');
+xLogin.addEventListener('click', () => {
+    login.classList.remove('loginForm');
+    login.classList.add('loginContainer');
+});
